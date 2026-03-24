a6eeedb191ea804e0ff8214747d6bfea62fb31fc
Sivanarayanareddy123 <159987628+Sivanaryanareddy123@users.noreply.github.com>
2026-03-24 18:50:55 +0530

    Update FinanceController.java

--- HealthCareHub/src/main/java/nimblix/in/HealthCareHub/controller/FinanceController.java
+++ HealthCareHub/src/main/java/nimblix/in/HealthCareHub/controller/FinanceController.java
@@ -60,25 +60,7 @@ public class FinanceController {
 
         return response;
         }
-    //  NEW - Daily Revenue API
-    @GetMapping("/revenue/daily")
-    public double getDailyRevenue() {
-
-        LocalDateTime now = LocalDateTime.now();
-        LocalDateTime startOfDay = now.toLocalDate().atStartOfDay();
-
-        return getRevenue(startOfDay, now);
-    }
-
-    //  NEW - Monthly Revenue API
-    @GetMapping("/revenue/monthly")
-    public double getMonthlyRevenue() {
-
-        LocalDateTime now = LocalDateTime.now();
-        LocalDateTime startOfMonth = now.withDayOfMonth(1).toLocalDate().atStartOfDay();
-
-        return getRevenue(startOfMonth, now);
-    }
+   
     private double getRevenue(LocalDateTime start, LocalDateTime end) {
 
         List<Payment> payments = entityManager
