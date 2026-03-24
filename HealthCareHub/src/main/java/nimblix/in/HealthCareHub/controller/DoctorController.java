7c736eb0b21324615bcbeaa682249a686d6d38e1
Mahmood Alam <mahmoodalam2601@gmail.com>
2026-03-24 12:18:29 +0530

    Added doctor availability and schedule APIs
    :wq

--- HealthCareHub/src/main/java/nimblix/in/HealthCareHub/controller/DoctorController.java
+++ HealthCareHub/src/main/java/nimblix/in/HealthCareHub/controller/DoctorController.java
@@ -1,6 +1,9 @@
 package nimblix.in.HealthCareHub.controller;
 
+import nimblix.in.HealthCareHub.service.DoctorAvailabilityService;
+
 import java.util.List;
+import nimblix.in.HealthCareHub.request.DoctorAvailabilityRequest;
 
 import nimblix.in.HealthCareHub.model.Review;
 import nimblix.in.HealthCareHub.request.DoctorRegistrationRequest;
@@ -9,7 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import nimblix.in.HealthCareHub.service.DoctorService;
-
 import nimblix.in.HealthCareHub.response.DoctorProfileResponse;
 
 @RestController
@@ -19,24 +21,22 @@ public class DoctorController {
     @Autowired
     private DoctorService doctorService;
 
+    @Autowired
+    private DoctorAvailabilityService doctorAvailabilityService;
+
     @GetMapping("/roles")
     public ResponseEntity<List<String>> getAllRoles() {
-
         return ResponseEntity.ok(doctorService.getAllRoles());
     }
 
     @PostMapping("/register")
-    public ResponseEntity<String> registerDoctor(@RequestBody DoctorRegistrationRequest request)
-    {
-         return ResponseEntity.ok(doctorService.registerDoctor(request));
+    public ResponseEntity<String> registerDoctor(@RequestBody DoctorRegistrationRequest request) {
+        return ResponseEntity.ok(doctorService.registerDoctor(request));
     }
 
     // to get doctor details by ID
     @GetMapping("/{doctorId}")
-    public ResponseEntity<DoctorProfileResponse> getDoctorById(@PathVariable Long doctorId)
-    {
-
-        //service to fetch doctor details
+    public ResponseEntity<DoctorProfileResponse> getDoctorById(@PathVariable Long doctorId) {
         DoctorProfileResponse doctor = doctorService.getDoctorById(doctorId);
         return ResponseEntity.ok(doctor);
     }
@@ -45,13 +45,22 @@ public class DoctorController {
     @PatchMapping("/{doctorId}")
     public ResponseEntity<String> updateDoctor(
             @PathVariable Long doctorId,
-            @RequestBody DoctorRegistrationRequest request)
-    {
-        // service layer to update doctor
-        String response = doctorService.updateDoctor(doctorId, request);
+            @RequestBody DoctorRegistrationRequest request) {
 
+        String response = doctorService.updateDoctor(doctorId, request);
         return ResponseEntity.ok(response);
     }
 
-
+    @PostMapping("/availability")
+    public ResponseEntity<String> setDoctorAvailability(@RequestBody DoctorAvailabilityRequest request) {
+        return ResponseEntity.ok(doctorAvailabilityService.setAvailability(request));
     }
+
+    @PostMapping("/{doctorId}/schedule")
+    public ResponseEntity<String> createDoctorSchedule(
+            @PathVariable Long doctorId,
+            @RequestBody List<DoctorAvailabilityRequest> scheduleRequests
+    ) {
+        return ResponseEntity.ok(doctorAvailabilityService.createSchedule(doctorId, scheduleRequests));
+    }
+}
